68147ff70a8c29388d05983a61cca24bad7ac539
majunjie58 <ma.junjie@decathlon.com>
2018-09-20 17:46:54 +0800

    Update app.js

--- chcReport/app.js
+++ chcReport/app.js
@@ -1,48 +1,28 @@
-var global_slice = -1;
-
+var canvasHeight = 0;
 $(".createPicture").click(function(){
     var node = document.querySelector(".scale-container");
     refreshHeight();
     html2canvas(node,{
         scale: 1.4
     }).then(function(canvas) {
-        var img = canvasToPicture(canvas);
         document.querySelector(".picturePopup").innerHTML = "";
-        var cHeight = canvas.height;
-        var cWidth = canvas.width;
-        var count = Math.ceil(cHeight / 1000);
-        global_slice = count;
+        document.querySelector(".picturePopup").appendChild(canvas);
         global_links = $(".square .links a");
-        img.onload = function(){
-            for(var i=0;i<count;i++){
-                var can = document.createElement("canvas");
-                can.width = cWidth;
-                can.height = i==0?cHeight%1000:1000;
-                can.setAttribute("data-src","");
-                var ctx =can.getContext('2d');
-                ctx.drawImage(img,0,i==0?0:(i-1)*1000+cHeight%1000,cWidth,i==0?cHeight%1000:1000,0,0,cWidth,i==0?cHeight%1000:1000);
-                document.querySelector(".picturePopup").appendChild(can);
-                can.ondblclick = function(){
-                    var link_temp = $(this).attr("data-src");
-                    var link = prompt("src:",link_temp);
-                    if(link == null){
-                        link = link_temp;
-                    }
-                    $(this).attr("data-src",link);
-                    return false;
-                }
+        canvas.setAttribute("data-src","");
+        canvasHeight = canvas.height * 700 / 1470 ;
+        console.log(canvasHeight);
+        canvas.ondblclick = function(){
+            var link_temp = $(this).attr("data-src");
+            var link = prompt("src:",link_temp);
+            if(link == null){
+                link = link_temp;
             }
+            $(this).attr("data-src",link);
+            return false;
         }
         
     });
 });
-
-function canvasToPicture(canvas) {
-    var imgUrl = canvas.toDataURL('image/png');
-    var img = new Image();
-    img.src = imgUrl;
-    return img;
-}
 
 function refreshData(data){
     for(var i=0;i<data.length;i++){
@@ -77,26 +57,28 @@ $(".links .link-item").each(function(){
 $(".button.createCode").click(function(){
     $(".code").addClass("codeShow");
     var text = '<center><table align="center" bgcolor="#f0f0f0" border="0" cellpadding="0" cellspacing="0" class="wrapper" style="margin: 0; padding: 0;" width="700"><tbody>';
-    var srcs = [];
-    $(".picturePopup canvas").each(function(){
-        var src = $(this).attr("data-src");
-        srcs.push(src);
-    });
+    var srcs = $(".picturePopup canvas").attr("data-src");
     var links = [];
     $(".scale-container .links a").each(function(){
         var link = $(this).attr("data-href");
         links.push(link);
     });
-    console.log(links);
-    for(var i=0;i<global_slice;i++){
-        if(i==global_slice-1){
-            text += '<tr><td><img usemap="#areamaps" style="width:100%;display:block;height: auto" src="'+srcs[i]+'">';
-            text += '<map name="areamaps" id="areamaps"><area shape="poly" href="'+links[0]+'" coords="40,340,135,325,135,350,40,365,40,340" /><area shape="poly" href="'+links[1]+'" coords="160,320,325,295,325,315,160,340,160,320" /><area shape="poly" href="'+links[2]+'" coords="40,370,135,355,135,380,40,395,40,370" /><area shape="poly" href="'+links[3]+'" coords="160,350,325,325,325,345,160,370,160,350" /></map>'
-            text += '</td></tr>';
-        }else{
-            text += '<tr><td><img style="width:100%;display:block;height: auto" src="'+srcs[i]+'"></td></tr>'
+    var coords = [
+        [40,340,135,325,135,350,40,365,40,340],
+        [160,320,325,295,325,315,160,340,160,320],
+        [40,370,135,355,135,380,40,395,40,370],
+        [160,350,325,325,325,345,160,370,160,350]
+    ]
+    for(var i=0;i<coords.length;i++){
+        for(var j=0;j<coords[i].length;j++){
+            if(j%2!=0){
+                coords[i][j] = canvasHeight - (475 - coords[i][j]);
+            }
         }
     }
+    text += '<tr><td><img usemap="#areamaps" style="width:100%;display:block;height: auto" src="'+srcs+'">';
+    text += '<map name="areamaps" id="areamaps"><area shape="poly" href="'+links[0]+'" coords="'+coords[0].toString()+'" /><area shape="poly" href="'+links[1]+'" coords="'+coords[1].toString()+'" /><area shape="poly" href="'+links[2]+'" coords="'+coords[2].toString()+'" /><area shape="poly" href="'+links[3]+'" coords="'+coords[3].toString()+'" /></map>'
+    text += '</td></tr>';
     text += '</tbody></table></center>';
     $(".code textarea").html('<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><meta name="viewport" content="width=device-width; initial-scale=1.0;" /><!-- CSS: START --><!-- CSS: END --></head><body>'+text+'</body></html>');
 })
